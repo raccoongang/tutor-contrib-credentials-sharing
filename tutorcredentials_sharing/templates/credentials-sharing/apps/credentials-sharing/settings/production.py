c81d982e5e5278476a914abd44c9d8211f7b87d5
Іван Нєдєльніцев <ivan.niedielnitsev@raccoongang.com>
2024-12-26 10:31:20 +0200

    fix: fixed sandbox usage of badge providers for prod settings

--- tutorcredentials_sharing/templates/credentials-sharing/apps/credentials-sharing/settings/production.py
+++ tutorcredentials_sharing/templates/credentials-sharing/apps/credentials-sharing/settings/production.py
@@ -2,4 +2,7 @@ from credentials.settings.production import *
 
 {% include "credentials-sharing/apps/credentials-sharing/settings/partials/common.py" %}
 
+BADGES_CONFIG['credly']['USE_SANDBOX'] = CONFIG_FILE.get('USE_CREDLY_SANDBOX', False)
+BADGES_CONFIG['accredible']['USE_SANDBOX'] = CONFIG_FILE.get('USE_ACCREDIBLE_SANDBOX', False)
+
 {{ patch("credentials-settings-production") }}
